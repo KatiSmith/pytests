0cecccd174bee6e320993474718fa569d4265888
Katerina <107837618+KatiSmith@users.noreply.github.com>
2023-02-26 20:42:23 +0300

    Update settings.py

--- APIPetFriends/settings.py
+++ APIPetFriends/settings.py
@@ -1,5 +1,5 @@
-valid_email = "teodor@gmail.com"
-valid_password = "290888Katya"
+valid_email = "mail@mail.ru"
+valid_password = "qwerty12345"
 
 not_valid_email = "blablabla@yandex.ru"
 not_valid_password = "123456789"
